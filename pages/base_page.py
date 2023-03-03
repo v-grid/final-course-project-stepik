73216f5a6513513f8b400d4222094f1650bd074d
v-grid <gridinvictor@gmail.com>
2023-03-03 13:18:34 +0300

    added checking for the presence of elements on the page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,17 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10): # метод, который вызывается, когда создаем объект
         self.browser = browser
         self.url = url
-    def open(self):
+        self.browser.implicitly_wait(timeout)
+
+    def open(self): # метожд открывает нужную страницу в браузере, используя метод get()
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what): # метод который перехватывает исключение. Передавать два аргумента: как искать (css, id, xpath и тд) и что искать (строку-селектор)
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -10,8 +10,16 @@ from selenium.webdriver.common.by import By'''
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()'''
 
+link_page = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link(browser):
+    link = link_page
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = link_page
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
